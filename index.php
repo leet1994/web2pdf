9c1c3ca3e4ebf27ad78f3db1ac5511f0b8530b06
leet1994 <59703010+leet1994@users.noreply.github.com>
2021-03-08 20:05:53 +0100

    [minor] small changes for own usage

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if (!isset($_GET['filename'])) die('No filename specified');
 require_once 'vendor/autoload.php';
 
 $url = filter_var($_GET['url'], FILTER_SANITIZE_URL);
-$filename = $string = preg_replace('/[^ \w]+/', '', $_GET['filename']);
+$filename = $string = preg_replace('/[^ \w\-\_\.]+/', '', $_GET['filename']);
 
 file_put_contents('log/requests.log', PHP_EOL . date('c') . ' - ' . $url . ' - ' . $filename, FILE_APPEND);
 
@@ -19,16 +19,33 @@ $bin = (PHP_INT_SIZE === 8) ?
 $snappy = new \Knp\Snappy\Pdf($root . $bin);
 
 $snappy->setOption('print-media-type', true);
-$snappy->setOption('disable-javascript', true);
+$snappy->setOption('images', true);
+$snappy->setOption('enable-javascript', true);
+$snappy->setOption('javascript-delay', 200);
 $snappy->setOption('lowquality', false);
+$snappy->setOption('enable-external-links', true);
+$snappy->setOption('enable-internal-links', true);
+$snappy->setOption('enable-forms', true);
+//$snappy->setOption('', true);
+
+$snappy->setOption('footer-left', $url);
+$snappy->setOption('footer-line', true);
+$snappy->setOption('footer-font-size', 8);
+
+$snappy->setOption('header-right', "Created on ".date("d.m.y"));
+$snappy->setOption('header-line', true);
+$snappy->setOption('header-font-size', 8);
 
 // Use $snappy->getOptions() to see all possible options
 // @see http://wkhtmltopdf.org/usage/wkhtmltopdf.txt
-// d($snappy->getOptions());exit;
+// var_dump($snappy->getOptions());exit;
 
 header("Content-Type: application/pdf");
-header("Content-Disposition: attachment; filename='" . $filename . ".pdf'");
+//header("Content-Disposition: attachment; filename=\"" . $filename . ".pdf\"");
+header("Content-Disposition: inline; filename=\"" . $filename . ".pdf\"");
 
 file_put_contents('log/requests.log', ' - OK', FILE_APPEND);
 
 echo $snappy->getOutput($url);
+
+?>
